a6672ef40df08731543517ea7225ae81e57fb8ca
FilipBotka <xbotkaf@stuba.sk>
2023-02-25 14:15:43 +0100

    datove typy

--- SkuskaGit/src/main/java/org/example/App.java
+++ SkuskaGit/src/main/java/org/example/App.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
+
+        int podiel = 2/3;
+        System.out.println(podiel);
+
+        int zvysok = 2%3;
+        System.out.println(zvysok);
+
+        float a = 2.0f/3;
+        System.out.println(a);
+
     }
 }
