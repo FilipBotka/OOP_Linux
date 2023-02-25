4924e7936a05c9e1fc09135ecb3695ac94d27165
FilipBotka <xbotkaf@stuba.sk>
2023-02-25 14:19:12 +0100

    if a vstupne argumenty

--- SkuskaGit/src/main/java/org/example/App.java
+++ SkuskaGit/src/main/java/org/example/App.java
@@ -2,16 +2,13 @@ package org.example;
 
 public class App {
     public static void main(String[] args){
-        //System.out.println("Hello World");
 
-        int podiel = 2/3;
-        System.out.println(podiel);
-
-        int zvysok = 2%3;
-        System.out.println(zvysok);
-
-        float a = 2.0f/3;
-        System.out.println(a);
+        int cislo = Integer.parseInt(args[0]);
+        if (cislo == 5) {
+            System.out.println("cislo je 5");
+        } else {
+            System.out.println("cislo nie je 5");
+        }
 
     }
 }
