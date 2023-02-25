f6dee1d58f79f01ea6c508003343213445199a95
FilipBotka <xbotkaf@stuba.sk>
2023-02-25 14:26:44 +0100

    for

--- SkuskaGit/src/main/java/org/example/App.java
+++ SkuskaGit/src/main/java/org/example/App.java
@@ -3,18 +3,18 @@ package org.example;
 public class App {
     public static void main(String[] args){
 
-        int cislo = 1;
-
-        switch (cislo) {
-            case 1:
-                System.out.println("nieco");
-                break;
-
-            case 2:
-                System.out.println("nieco 2");
-            default:
-                System.out.println("Ani jedno z toho");
+        //int cislo = 1;
+        for (int i=1; i<3; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("nieco");
+                    break;
+                case 2:
+                    System.out.println("nieco 2");
+                    break;
+                default:
+                    System.out.println("Ani jedno z toho");
+            }
         }
-
     }
 }
